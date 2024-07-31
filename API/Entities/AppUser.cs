061719bea7e116d043eb369f305662be101702e1
juan <142954292+charsplusjk@users.noreply.github.com>
2024-07-31 20:30:43 -0300

    Extending the API

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -33,10 +33,5 @@ public class AppUser
     public required string Country { get; set; }
 
     public List<Photo> Photos { get; set; } = [];
-
-    public int GetAge()
-    {
-        return DateOfBirth.CalculateAge();
-    }
 }
 
